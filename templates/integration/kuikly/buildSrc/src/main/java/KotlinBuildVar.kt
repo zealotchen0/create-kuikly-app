87ecb98c332d2548e39dfd6a4dae4041929f505e
elixxli <elixxli@tencent.com>
2026-07-24 14:26:39 +0800

    chore: update kuikly version

--- templates/integration/kuikly/buildSrc/src/main/java/KotlinBuildVar.kt
+++ templates/integration/kuikly/buildSrc/src/main/java/KotlinBuildVar.kt
@@ -1,6 +1,6 @@
 object Version {
 
-    private const val KUIKLY_VERSION = "2.7.0"
+    private const val KUIKLY_VERSION = "2.22.0"
     private const val KOTLIN_VERSION = "2.1.21"
     private const val KOTLIN_OHOS_VERSION = "2.0.21-ohos"
 
